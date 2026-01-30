7b1f8c243f87dbe518f55bb4d3b6eb6094f990a1
subsurfaceiodev <subsurfaceiodev@gmail.com>
2026-01-30 16:24:04 -0500

    Merge remote-tracking branch 'origin/master'

--- src/dynamic_docstrings/extension.py
+++ src/dynamic_docstrings/extension.py
@@ -14,8 +14,6 @@ class DynamicDocstrings(griffe.Extension):
             loader: griffe.GriffeLoader,
             **kwargs,
     ) -> None:
-        logger.warning(f"FUNC PATH: {func.parent.path}")
-        logger.warning(self.object_paths)
         if self.object_paths and func.parent.path not in self.object_paths:
             return  # Skip objects that were not selected.
 
@@ -29,9 +27,11 @@ class DynamicDocstrings(griffe.Extension):
         except AttributeError:
             logger.debug(f"Object {func.path} does not have a __doc__ attribute")
             return
-
+        logger.warning(f"FUNC PATH: {func.parent.path}")
+        logger.warning(self.object_paths)
         if not func.name.startswith('_'):
             docstring = docstrings[func.parent.path.split('.')[-1]][func.name]
+            logger.warning(docstring)
             func.docstring = griffe.Docstring(
                 docstring,
                 parent=func,
