b25b4af6855ae863cd6cf5b78e34a7cb91e02d57
subsurfaceiodev <subsurfaceiodev@gmail.com>
2026-01-30 16:30:19 -0500

    Merge remote-tracking branch 'origin/master'

--- src/dynamic_docstrings/extension.py
+++ src/dynamic_docstrings/extension.py
@@ -17,23 +17,18 @@ class DynamicDocstrings(griffe.Extension):
         if self.object_paths and func.parent.path not in self.object_paths:
             return  # Skip objects that were not selected.
 
-        # Import object to get its evaluated docstring.
-        logger.warning(f"FUNC PATH: {func.parent.path}")
-        logger.warning(self.object_paths)
-        try:
-            runtime_obj = griffe.dynamic_import(func.path)
-            docstring = runtime_obj.__doc__
-        except ImportError:
-            logger.debug(f"Could not get dynamic docstring for {func.path}")
-            return
-        except AttributeError:
-            logger.debug(f"Object {func.path} does not have a __doc__ attribute")
-            return
         if not func.name.startswith('_'):
-            docstring = docstrings[func.parent.path.split('.')[-1]][func.name]
-            func.docstring = griffe.Docstring(
-                docstring,
-                parent=func,
-                parser=loader.docstring_parser,
-                parser_options=loader.docstring_options,
-            )
+            return
+
+        try:
+            module_key = func.parent.path.split('.')[-1]
+            docstring = docstrings[module_key][func.name]
+        except KeyError:
+            return
+
+        func.docstring = griffe.Docstring(
+            docstring,
+            parent=func,
+            parser=loader.docstring_parser,
+            parser_options=loader.docstring_options,
+        )
